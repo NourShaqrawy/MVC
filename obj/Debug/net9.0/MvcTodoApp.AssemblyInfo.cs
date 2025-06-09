156bd1753056263e530132e52015e1f5947f3175
NourShaqrawy <shkrawysaleh037@gmail.com>
2025-06-09 07:13:46 +0300

    .

--- obj/Debug/net9.0/MvcTodoApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcTodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcTodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6404b2582dcd8d3b7d475096bf848f8b1a58332")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcTodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcTodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
